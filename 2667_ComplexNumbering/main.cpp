4c5a22aac7791ea749f63c638f538322ddf3ea0b
literate-t <gaekoon@gmail.com>
2025-06-01 02:21:58 +0900

    add: 백준 2667번 단지번호 붙이기
    - https://www.acmicpc.net/problem/2667

---
+++ 2667_ComplexNumbering/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+bool map[25 + 2][25 + 2];
+bool visited[25 + 2][25 + 2];
+int N;
+int adj_dir_row[] = { 1, 0, -1, 0 };
+int adj_dir_col[] = { 0, 1, 0, -1 };
+
+void answer();
+void dfs(int row, int col);
+
+int pot_count = 0;
+
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		string line;
+		cin >> line;
+		for (int j = 0; j < N; ++j)
+			map[i][j] = line[j] - '0';
+	}
+
+	answer();
+
+	cout << pq.size() << endl;
+	while (!pq.empty())
+	{
+		cout << pq.top() << endl;
+		pq.pop();
+	}
+}
+
+void answer()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			if (map[i][j] && visited[i][j] == false)
+			{
+				dfs(i, j);
+				pq.push(pot_count);
+				pot_count = 0;
+			}
+		}
+	}
+}
+
+void dfs(int row, int col)
+{
+	if (visited[row][col]) return;
+
+	++pot_count;
+
+	visited[row][col] = true;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int next_row = row + adj_dir_row[i];
+		int next_col = col + adj_dir_col[i];
+
+		if (map[next_row][next_col] && false == visited[next_row][next_col])
+			dfs(next_row, next_col);
+	}
+}
