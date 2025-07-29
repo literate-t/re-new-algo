e9b51450b1b48089af3fc4f11e520bc3b5926a1d
literate-t <gaekoon@gmail.com>
2025-07-30 01:57:15 +0900

    add: 백준 2293 동전1을 추가한다. - https://www.acmicpc.net/problem/2293

---
+++ 2293_Coin1/2293_Coin1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int coins[101]{};
+int dp[10001]{};
+
+int main()
+{
+    int n, k;
+
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)
+        cin >> coins[i];
+
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        int coin = coins[i];
+        for (int j = coin; j <= k; ++j)
+        {
+            dp[j] += dp[j - coin];
+        }
+    }
+
+    cout << dp[k] << '\n';
+}
