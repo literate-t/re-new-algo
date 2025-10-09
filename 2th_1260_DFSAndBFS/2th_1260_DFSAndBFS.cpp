5e626b7a4cdc9f7e871bfca0c1fb72503bfa9999
literate-t <gaekoon@gmail.com>
2025-10-09 11:41:40 +0900

    feat: [다시풀기] 백준 1260을 dfs로 풀기
    - bfs 추가해야 한다

---
+++ 2th_1260_DFSAndBFS/2th_1260_DFSAndBFS.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+bool nodes[1001][1001]{};
+bool visited[1001]{};
+int N, M, V;
+
+void dfs(int start);
+
+int main()
+{
+	cin >> N >> M >> V;
+	for (int i = 0; i < M; ++i)
+	{
+		int s, e;
+		cin >> s >> e;
+		nodes[s][e] = true;
+		nodes[e][s] = true;
+	}
+
+	dfs(V);
+}
+
+void dfs(int start)
+{
+	visited[start] = true;
+	cout << start << " ";
+
+	for (int i = 1; i <= N; ++i)
+		if (nodes[start][i] == true && visited[i] == false)
+			dfs(i);
+}
