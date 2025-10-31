d53e95d52107616de905acb0585b1ca4b36b5d8b
literate-t <gaekoon@gmail.com>
2025-10-31 16:39:59 +0900

    add: 백준 9095번 - 1,2,3 더하기 - dp - https://www.acmicpc.net/problem/9095

---
+++ 9095_PlusOneTwoThree/9095_PlusOneTwoThree.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    int dp[11]{};
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (int i = 4; i <= 10; ++i)
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1] ;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << dp[N] << '\n';
+    }
+}
