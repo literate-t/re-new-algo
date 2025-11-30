b31201ae5a2b6bb9db8eaf777a16d6d18d0998f8
literate-t <gaekoon@gmail.com>
2025-12-01 02:27:26 +0900

    feat: 백준 1697 - 숨바꼭질 - https://www.acmicpc.net/problem/1697 - 이니셜라이저를 활용한 반복문이 kick

---
+++ 2th_1697_HideAndSeek/2th_1697_HideAndSeek.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int N, K;
+constexpr int kMax = 100001;
+int position[kMax]{};
+queue<int> q;
+int main()
+{
+	scanf("%d%d", &N, &K);
+	q.push(N);
+
+	while (!q.empty())
+	{
+		auto n = q.front(); q.pop();
+		if (n == K)
+		{
+			printf("%d\n", position[n]);
+			return 0;
+		}
+
+		// initializer
+		for (int next : {n - 1, n + 1, n * 2})
+		{
+			if (next < 0 || next >= kMax || position[next] != 0)
+				continue;
+
+			q.push(next);
+			position[next] = position[n] + 1;
+		}
+
+		//if (n >= 1 && position[n - 1] == 0)
+		//{
+		//	q.push(n - 1);
+		//	position[n - 1] = position[n] + 1;
+		//}
+		//if (n + 1 <= 100000 && position[n + 1] == 0)
+		//{
+		//	q.push(n + 1);
+		//	position[n + 1] = position[n] + 1;
+		//}
+		//if (n * 2 <= 100000 && position[n * 2] == 0)
+		//{
+		//	q.push(n * 2);
+		//	position[n * 2] = position[n] + 1;
+		//}
+	}
+}
