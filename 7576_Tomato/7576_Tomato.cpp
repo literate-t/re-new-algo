ce6565980e83a139f2e76836a8b6c031dc6085be
literate-t <gaekoon@gmail.com>
2025-11-30 18:25:53 +0900

    feat: 백준 7576 - 토마토 - https://www.acmicpc.net/problem/7576 - 랭크 보면 성능이 나쁘진 않은데 엄청 좋지도 않으니 개선해볼 것

---
+++ 7576_Tomato/7576_Tomato.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <queue>
+int map[1000][1000]{};
+int adjacent_m[] = { 0, 1, 0, -1 };
+int adjacent_n[] = { 1, 0, -1, 0 };
+std::queue<std::pair<std::pair<int, int>, int>> q;
+int M, N;
+int main()
+{
+	scanf("%d%d", &M, &N);
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			int t;
+			scanf("%d", &t);
+			map[i][j] = t;
+			if (t == 1)
+				q.push({ {i, j}, 0});
+		}
+	}
+
+	int answer = 0;
+	while (!q.empty())
+	{
+		auto [pos, count] = q.front(); q.pop();
+		auto [n, m] = pos;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			int next_m = m + adjacent_m[i];
+			int next_n = n + adjacent_n[i];
+			if (next_m < 0 || next_m >= M || next_n < 0 || next_n >= N)
+				continue;
+
+			if (map[next_n][next_m] == 0)
+			{
+				map[next_n][next_m] = 1;
+				q.push({ {next_n, next_m}, count + 1 });
+			}
+		}
+		answer = count;
+	}
+
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			if (map[i][j] == 0)
+			{
+				printf("-1\n");
+				return 0;
+			}
+	
+	if (answer == 0)
+		printf("0\n");
+	else
+		printf("%d\n", answer);
+}
